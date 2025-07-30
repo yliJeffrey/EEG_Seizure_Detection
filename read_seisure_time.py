48fecefbe8d4f2bd038dd481db9b22cdbbfb2707
yliJeffrey <yuanzhu.li@outlook.com>
2025-07-30 15:58:03 -0400

    first commit

---
+++ read_seisure_time.py
@@ -0,0 +1,16 @@
+import pickle
+
+# Define the path to your pickle file
+file_path = 'seizure_times.pkl' 
+
+# 'rb' means 'read binary' mode
+with open(file_path, 'rb') as file:
+    # Load the data from the file
+    loaded_data = pickle.load(file)
+
+n = 0
+for test,time in loaded_data.items():
+    print(f"{test}: {time}")
+    n += len(time)
+
+print(n)
